20e5cbc635f5956b870628851e009e61f8f9d5d1
Fakhri <fakhri3d891@gmail.com>
2025-08-13 15:19:12 +0700

    menmabhakan 2 mode drak dan white

--- main.js
+++ main.js
@@ -1,33 +1,41 @@
-const display = document.getElementById('display')
-const buttons = document.querySelectorAll('.btn')
-
+const display = document.getElementById('display');
+const buttons = document.querySelectorAll('.btn');
+const toggleBtn = document.getElementById("toggleTheme");
 
 let expression = "";
 
-buttons.forEach(
-    button => {
-        button.addEventListener('click',() =>{
-            const value =button.getAttribute('data-value')
+// Kalkulator
+buttons.forEach(button => {
+    button.addEventListener('click', () => {
+        const value = button.getAttribute('data-value');
 
-            if(value === 'AC'){
-                expression = ''
-                display.value = ''
-            }else if (value === 'DEL') {
-                expression = expression.slice(0, -1)
-                display.value = expression
-            }else if (value === '='){
-                try{
-                    expression = eval(expression).toString()
-                    display.value = expression
-                }catch (error){
-                    display.value = 'error'
-                    expression = ''
-                }
+        if (value === 'AC') {
+            expression = '';
+            display.value = '';
+        } else if (value === 'DEL') {
+            expression = expression.slice(0, -1);
+            display.value = expression;
+        } else if (value === '=') {
+            try {
+                expression = eval(expression).toString();
+                display.value = expression;
+            } catch (error) {
+                display.value = 'error';
+                expression = '';
             }
-            else{
-                expression+= value
-                display.value = expression
-            }
-        })
+        } else {
+            expression += value;
+            display.value = expression;
+        }
+    });
+});
+
+// Toggle tema
+toggleBtn.addEventListener("click", () => {
+    document.body.classList.toggle("light");
+    if (document.body.classList.contains("light")) {
+        toggleBtn.textContent = "Dark Mode";
+    } else {
+        toggleBtn.textContent = "White Mode";
     }
-)
+});
